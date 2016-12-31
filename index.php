a946ae4a17122c09bb9e04abf18b056d6b848763
officialmansu <officialmansu@gmail.com>
2016-12-31 21:31:06 +0900

    s
    s

--- index.php
+++ index.php
@@ -4,11 +4,17 @@
 		<meta charset="utf-8">
 		<title>Hello, World!</title>
 	</head>
-	<link rel="stylesheet" href="/css/color.css">
+	<!--<link rel="stylesheet" href="/css/color.css">-->
+	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700" rel="stylesheet">
 	<style media="screen">
 		body {
 			margin: 0;
 			min-height: 100vh;
+			background-image: url(https://source.unsplash.com/random/1600x900);
+			background-size: cover;
+			background-repeat: no-repeat;
+
+			font-family: 'Source Sans Pro', sans-serif;
 		}
 
 		.container {
@@ -22,6 +28,17 @@
 		  /*background: lime;*/
 		}
 
+		.title-container {
+			padding: 1em;
+
+			background: -moz-linear-gradient(top,  rgba(0,0,0,0.65) 0%, rgba(0,0,0,0) 100%); /* FF3.6-15 */
+			background: -webkit-linear-gradient(top,  rgba(0,0,0,0.65) 0%,rgba(0,0,0,0) 100%); /* Chrome10-25,Safari5.1-6 */
+			background: linear-gradient(to bottom,  rgba(0,0,0,0.65) 0%,rgba(0,0,0,0) 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+			filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#a6000000', endColorstr='#00000000',GradientType=0 ); /* IE6-9 */
+			color: rgb(204, 204, 204);
+
+		}
+
 		.title-image-container {
 			float: left;
 			/*position: fixed;*/
@@ -29,8 +46,8 @@
 		}
 
 		.title-text-container {
-			/*margin-left: 2rem;*/
-			/*padding: 2em;*/
+			margin-left: 3.5rem;
+			padding-top: .5em;
 			height: 6em;
 
 		}
@@ -39,6 +56,7 @@
 			/*padding-top: .5rem;*/
 			/*position: relative;*/
 			font-size: 3.5em;
+			margin-left: .3em;
 
 		}
 
@@ -49,10 +67,17 @@
 		}
 
 		.article-container {
+			padding-top: .7em;
+			padding-bottom: .9em;
 			text-align: center;
 			top: 50%;
 			position: fixed;
 			transform: translateY(-50%);
+			padding-left: 2em;
+			background-color: rgba(255, 255, 255, 0.75);
+			-webkit-box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.75);
+			-moz-box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.75);
+			box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.75);
 		}
 
 		.article-title-text {
@@ -71,6 +96,8 @@
 			/*top: 50%;*/
 			/*transform: translateY(-50%);*/
 			max-width: 50rem;
+			margin: 0;
+
 		}
 
 	</style>
