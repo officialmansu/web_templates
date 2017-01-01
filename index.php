ecf3b062a10461dbf9daa2c59efba5db129ef47f
officialmansu <officialmansu@gmail.com>
2017-01-01 23:18:53 +0900

    2017-01-01_11:18_PM
    중간 완료

--- index.php
+++ index.php
@@ -2,115 +2,13 @@
 <html>
 	<head>
 		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
 		<title>Hello, World!</title>
 	</head>
 	<!--<link rel="stylesheet" href="/css/color.css">-->
+	<link rel="stylesheet" href="/css/style.css">
 	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700" rel="stylesheet">
-	<style media="screen">
-		body {
-			margin: 0;
-			min-height: 100vh;
-			background-image: url(https://source.unsplash.com/random/1600x900);
-			background-size: cover;
-			background-repeat: no-repeat;
-			font-family: 'Source Sans Pro', sans-serif;
-		}
-
-		.container {
-			position: fixed;
-		  top: 0;
-		  left: 0;
-		  bottom: 0;
-		  right: 0;
-		  overflow: auto;
-		}
-
-		.title-container {
-			padding: 1em;
-			background: -moz-linear-gradient(top,  rgba(0,0,0,0.65) 0%, rgba(0,0,0,0) 100%); /* FF3.6-15 */
-			background: -webkit-linear-gradient(top,  rgba(0,0,0,0.65) 0%,rgba(0,0,0,0) 100%); /* Chrome10-25,Safari5.1-6 */
-			background: linear-gradient(to bottom,  rgba(0,0,0,0.65) 0%,rgba(0,0,0,0) 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
-			filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#a6000000', endColorstr='#00000000',GradientType=0 ); /* IE6-9 */
-			color: rgb(204, 204, 204);
-
-		}
-
-		.title-image-container {
-			float: left;
-		}
-
-		.title-text-container {
-			margin-left: 3.5rem;
-			padding-top: .5em;
-			height: 6em;
-		}
-
-		.title {
-			font-size: 3.5em;
-			margin-left: .3em;
-			transition: color .5s ease;
-		}
-
-		.title:hover {
-			color: white;
-		}
-
-		.image {
-			width: 5em;
-			height: 5em;
-			padding: .5rem;
-			background-repeat: no-repeat;
-			background-size: cover;
-			transition: background-image .5s ease-in-out;
-			background-image: url("img/fb.png");
-		}
-
-		.image:hover {
-			background-image: url("img/fb(hovered).png");
-		}
-
-		.article-container {
-			padding-right: 1em;
-			padding-top: .7em;
-			padding-bottom: .9em;
-			text-align: center;
-			top: 50%;
-			position: fixed;
-			transform: translateY(-50%);
-			padding-left: 2em;
-			background-color: rgba(255, 255, 255, 0.75);
-			transition: background-color .5s ease;
-			-webkit-box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.75);
-			-moz-box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.75);
-			box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.75);
-		}
-
-		.article-container:hover {
-			background-color: rgba(255, 255, 255, 1);
-		}
-
-		.article-title-text {
-			text-align: left;
-			font-size: 2rem;
-			margin: 0;
-		}
-
-		.article {
-			text-align: left;
-			max-width: 50rem;
-			margin: 0;
-		}
-
-		.license {
-			text-decoration: none;
-			color: blue;
-			transition: color .5s ease;
-		}
-
-		.license:hover {
-			color: gray;
-		}
-	</style>
 	<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
 	<body>
 		<div class="container">
@@ -128,6 +26,7 @@
 						흠.. 이것은 단순한 예제인 것 같군요.. 아마 당신은 이 예제를 보고 아주 많은 영감을 받았을 것 같지만 알게 뭐야. 내 이름은 만수인데. 무려 가득 찰 만(滿)에 물 수(水) 자에요! 정말 놀랍지 않나요? 뭐 이름에 물 수(水) 자가 들어간다는 것 하나만으로도 알 수 있죠. 아무튼, 이 예제는 오픈소스는 아니지만 사용하실 때 제 이름을 꼭 언급해 주세요! 아 참, 그리고 이건 수익 창출을 목적으로 하는 프로젝트에는 사용할 수 없어요. 그리고 제가 한 말을 같이 써주셔야 해요. 알았죠? <br/><a href="https://creativecommons.org/licenses/by-nc-sa/4.0/" target="_blank" class="license">(CC BY-NC-SA 4.0)</a>
 					</p>
 				</article>
+				<div class="bottom-container"></div>
 			</div>
 		</div>
 	</body>
